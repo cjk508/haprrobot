ad8164fc83dfe31ec44041266d9c25bb6d39f58a
Chris King <cjk508@york.ac.uk>
2013-01-17 16:04:26 +0000

    stub created for initialiseSensors

---
+++ sensors.c
@@ -0,0 +1,21 @@
+/**
+* This method initialises the Sensors by setting 
+* up the pins on the MBED board
+*
+* @author Christopher King
+*/
+
+void initialiseSensors()
+{
+}
+
+
+/**
+* Main function that controls the initialisation
+*
+* @author Christopher King
+*/
+void main()
+{
+	initialiseSensors();
+}
