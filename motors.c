bc4ee9098bc1328701a45a30ea1d5b1a230b1830
Jed <jwm511@york.ac.uk>
2013-01-17 17:04:56 +0000

    Added basic to motors.c

---
+++ motors.c
@@ -0,0 +1,81 @@
+
+
+// 0xC1 - M1 forward
+// 0xC2 - M1 backward
+// 0xC5 - M2 forward
+// 0xC6 - M2 backward
+// robotCommand(hex, data)
+int current_motor_speed_left;
+int current_motor_speed_right;
+
+
+void speedCheck(int speed) {
+	if(speed > 25) speed = 25;
+}
+
+
+void setLeft(int left) {
+	current_motor_speed_left = left;
+	if(left < 0 ) {
+		left = left*-1;
+		speedCheck(left);
+		robotCommand(0xC2, left);
+	}
+	else {
+		speedCheck(left);
+		robotCommand(0xC1, left);
+	}
+}
+
+void setRight(int right) {
+	current_motor_speed_right = right;
+	if(right < 0 ) {
+		left = left*-1;
+		speedCheck(right);
+		robotCommand(0xC2, right);
+	}
+	else {
+		speedCheck(right);
+		robotCommand(0xC1, right);
+	}
+}
+
+void setMotors(int lm, int rm) {
+	setLeft(lm);
+	setRight(rm);
+	//uart.c stuff to do here
+	
+}
+
+void forwards(int lm, int rm) {
+	setMotors(lm, rm);
+
+}
+
+void backwards(int lm, int rm) {
+	setMotors(lm, rm);
+}
+
+void slowDown() {
+
+void right() {
+	setMotors(15, 0);
+}
+
+void left() {
+	setMotors(0, 15);
+}
+
+void brake() {
+	setMotors(0, 0);
+}
+
+void spinLeft() {
+	setMotors(-25, 25);
+
+}
+
+void spinRight() {
+	setMotors(25, -25);
+}
+
