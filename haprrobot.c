27115f9277e1fdd9a7755cfd7634afd0ad76c27c
Andy Durant <andy@ajdurant.co.uk>
2013-01-17 15:15:54 +0000

    base file

---
+++ haprrobot.c
@@ -0,0 +1,16 @@
+#include "lpc17xx_uart.h"		// Central include files
+#include "debug_frmwrk.h"
+#include "lpc17xx_pinsel.h"
+#include "lpc17xx_nvic.h"
+#include "lpc17xx_timer.h"
+#include "lpc_types.h"
+#include "lpc17xx_gpio.h"
+#include "lpc17xx_i2c.h"
+#include "lpc17xx_libcfg_default.h"
+#include "lpc17xx_adc.h"
+#include "lpc17xx_dac.h"
+
+#include "lpc17xx.h"
+#include "math.h"
+
+#define DEBUG 1
