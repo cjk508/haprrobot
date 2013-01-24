3408bf3ff5c6a20138b693659d218b69d2b1fee1
Chris King <cjk508@york.ac.uk>
2013-01-24 15:43:19 +0000

    Get functions for the left, right and front sensors now implemented. Change guard removed as it wasn't effective

--- sensors.c
+++ sensors.c
@@ -38,35 +38,50 @@ void pinConfSetup(uint8_t p_Portnum, uint8_t p_Pinnum, uint8_t p_Funcnum, uint8_
 	PINSEL_ConfigPin(&PinCfg);
 }
 
-struct SensorPair getSensorValues(int sensors[])
+struct SensorPair getLeftSensorValues()
 {
 	struct SensorPair returnValue;
-	returnValue.FrontSensor = 0;
-	returnValue.RearSensor = 0;	
+	returnValue.FrontSensor = currentReadings[0];
+	returnValue.RearSensor = currentReadings[2];	
 	return returnValue;
+}
+
+struct SensorPair getRightSensorValues()
+{
+	struct SensorPair returnValue;
+	returnValue.FrontSensor = currentReadings[1];
+	returnValue.RearSensor = currentReadings[3];	
+	return returnValue;
+}
+
+uint16_t getFrontSensorValue()
+{
+	return currentReadings[4];
 }
 /**
 * This method initialises the Sensors by setting 
 * up the pins on the MBED board
 *
 * @author Christopher King
-*/
+*/ 
 void initialiseSensors()
 {
 	// configures the analogue pins
 	pinConfSetup(PINSEL_PORT_0, analogSensorPins[0], PINSEL_FUNC_1, PINSEL_PINMODE_PULLUP,PINSEL_PINMODE_NORMAL);
 	pinConfSetup(PINSEL_PORT_0, analogSensorPins[1], PINSEL_FUNC_1, PINSEL_PINMODE_PULLUP,PINSEL_PINMODE_NORMAL);	
 	pinConfSetup(PINSEL_PORT_0, analogSensorPins[2], PINSEL_FUNC_1, PINSEL_PINMODE_PULLUP,PINSEL_PINMODE_NORMAL);	
-	pinConfSetup(PINSEL_PORT_0, analogSensorPins[3], PINSEL_FUNC_3, PINSEL_PINMODE_PULLUP,PINSEL_PINMODE_NORMAL);	
+	pinConfSetup(PINSEL_PORT_1, analogSensorPins[3], PINSEL_FUNC_3, PINSEL_PINMODE_PULLUP,PINSEL_PINMODE_NORMAL);	
 
 	GPIO_SetDir(0, frontSensor, 0);
 	GPIO_ClearValue(1, frontSensor);
 
 	// Set up the ADC sampling at 200kHz (maximum rate).
 	ADC_Init(LPC_ADC, 200000);
-
-	// Enable ADC channel 1.
+	
+	ADC_ChannelCmd(LPC_ADC, ADC_CHANNEL_0, ENABLE);
 	ADC_ChannelCmd(LPC_ADC, ADC_CHANNEL_1, ENABLE);
+	ADC_ChannelCmd(LPC_ADC, ADC_CHANNEL_2, ENABLE);
+	ADC_ChannelCmd(LPC_ADC, ADC_CHANNEL_4, ENABLE);
 
 	// Set ADC to continuously sample.
 	ADC_StartCmd (LPC_ADC, ADC_START_CONTINUOUS);
@@ -84,45 +99,20 @@ void initialiseSensors()
 void ADC_IRQHandler(void)
 {
 	// changed value means that if anything has changed since the last reading then it'll print out the changes
-	int changed = 0;
-	if(currentReadings[0] != ADC_ChannelGetData(LPC_ADC,ADC_CHANNEL_0))
+	int counter = 0;
+	uint16_t newReadings[] = {ADC_ChannelGetData(LPC_ADC,ADC_CHANNEL_0),ADC_ChannelGetData(LPC_ADC,ADC_CHANNEL_4),ADC_ChannelGetData(LPC_ADC,ADC_CHANNEL_1),ADC_ChannelGetData(LPC_ADC,ADC_CHANNEL_2),GPIO_ReadValue(frontSensor)};
+
+	while(counter < 5)
 	{
-		currentReadings[0] = ADC_ChannelGetData(LPC_ADC,ADC_CHANNEL_0);
-		changed = 1;
+		currentReadings[counter] = newReadings[counter];
+		counter = counter+1;
 	}
-	
-	if(currentReadings[1] != ADC_ChannelGetData(LPC_ADC,ADC_CHANNEL_3))
-	{
-		currentReadings[1] = ADC_ChannelGetData(LPC_ADC,ADC_CHANNEL_3);
-		changed = 1;
-	}
-	
-	if(currentReadings[2] != ADC_ChannelGetData(LPC_ADC,ADC_CHANNEL_1))
-	{
-		currentReadings[2] = ADC_ChannelGetData(LPC_ADC,ADC_CHANNEL_1);
-		changed = 1;
-	}
-	
-	if(currentReadings[3] != ADC_ChannelGetData(LPC_ADC,ADC_CHANNEL_2))
-	{
-		currentReadings[3] = ADC_ChannelGetData(LPC_ADC,ADC_CHANNEL_2);
-		changed = 1;
-	}				
-	if (currentReadings[4] != GPIO_ReadValue(frontSensor))
-	{
-		currentReadings[4] = GPIO_ReadValue(frontSensor);
-		changed = 1;
-	}	
-	
-	if(changed == 1)
-	{
-		_DBG_("#################");
-		_DBG("FL:");	_DBD16(currentReadings[0]); _DBG_("");
-		_DBG("BL:");	_DBD16(currentReadings[2]); _DBG_("");
-		_DBG("FR:");	_DBD16(currentReadings[1]); _DBG_("");
-		_DBG("BR:");	_DBD16(currentReadings[3]); _DBG_("");			
-		_DBG("F:");	_DBD16(currentReadings[4]);_DBG_("");	
-		_DBG_("#################");	
-	}
+
+	_DBG_("#################");
+	_DBG("FL:");	_DBD16(currentReadings[0]); _DBG_("");
+	_DBG("BL:");	_DBD16(currentReadings[2]); _DBG_("");
+	_DBG("FR:");	_DBD16(currentReadings[1]); _DBG_("");
+	_DBG("BR:");	_DBD16(currentReadings[3]); _DBG_("");			
+	_DBG("F:");	_DBD16(currentReadings[4]);_DBG_("");	
 	GPIO_ClearValue(1, frontSensor);			
 }
