2a79036ec343d02b3f22f0d8cc1c894d13b4bb6b
Andy Durant <andy@ajdurant.co.uk>
2013-01-31 15:14:38 +0000

    fixxed motors and started header file

--- motors.c
+++ motors.c
@@ -1,57 +1,57 @@
+#include "motors.h"
 
-// 0xC1 - M1 forward
-// 0xC2 - M1 backward
-// 0xC5 - M2 forward
-// 0xC6 - M2 backward
-// robotCommand(hex, data)
+// uint32_t cmdLeftMFw(int speed); 0xC1 - M1 forward
+// uint32_t cmdLeftMBw(int speed); 0xC2 - M1 backward
+// uint32_t cmdRightMFw(int speed); 0xC5 - M2 forward
+// uint32_t cmdRightMBw(int speed); 0xC6 - M2 backward
+
 int current_motor_speed_left;
 int current_motor_speed_right;
 
 
-void speedCheck(int speed) {
-	if(speed > 25) speed = 25;
+int speedCheck(int speed) {
+	if(speed > MAX_SPEED) speed = MAX_SPEED;
+	return speed;
 }
 
-
-void se
-tLeftMotor(int left) {
+void setLeftMotor(int left) {
 	current_motor_speed_left = left;
 	if(left < 0 ) {
-		left = left*-1;
-		speedCheck(left);
-		robotCommand(0xC2, left);
+		left = left*(-1);
+		cmdLeftMBw(speedCheck(left));
+		cmdLeftMFw(0);
 	}
 	else {
-		speedCheck(left);
-		robotCommand(0xC1, left);
+		cmdLeftMFw(speedCheck(left));
+		cmdLeftMBw(0);
 	}
 }
 
 void setRightMotor(int right) {
 	current_motor_speed_right = right;
 	if(right < 0 ) {
-		right = right*-1;
-		speedCheck(right);
-		robotCommand(0xC2, right);
+		right = right*(-1);
+		cmdRightMBw(speedCheck(left));
+		cmdRightMFw(0);
 	}
 	else {
 		speedCheck(right);
-		robotCommand(0xC1, right);
+		cmdRightMFw(speedCheck(left));
+		cmdRightMBw(0);
 	}
 }
 
 void setMotors(int lm, int rm) {
 	setLeftMotor(lm);
 	setRightMotor(rm);
-	//uart.c stuff to do here
 }
 
 void forwards(int s) {
 	setMotors(s, s);
-
 }
 
 void backwards(int s) {
+  s = s*(-1);
 	setMotors(s, s);
 }
 
@@ -73,20 +73,24 @@ void left() {
 	setMotors(0, 15);
 }
 
-void brake() {
+void freewheel() {
 	setMotors(0, 0);
+}
+
+void brake() {
+  int brakeSpeed = speedCheck(127);
+	cmdLeftMFw(brakeSpeed);
+	cmdRightMFw(brakeSpeed);
+	cmdLeftMBw(brakeSpeed);
+	cmdRightMBw(brakeSpeed);
 }
 
 void spinLeft() {
 	setMotors(-25, 25);
-
 }
 
 void spinRight() {
 	setMotors(25, -25);
 }
 
-void delay() {
-	20000*20000;
-}
 
---
+++ motors.h
@@ -0,0 +1,65 @@
+/* Motor library for m3pi
+ * Jed Warwick-Mooney
+ * Andrew Durant
+ * 
+ * Provides basic functions for each serial command that can be sent to the m3pi
+ *
+ */
+
+#ifndef MOTOR_INC
+#define MOTOR_INC
+
+#include "uart.h"
+
+#define MAX_SPEED 25
+
+int current_motor_speed_left;
+int current_motor_speed_right;
+
+/**
+ * Restricts the max speed to the constant MAX_SPEED
+ *
+ * @author Jed Warwick-Mooney
+ * @author Andrew Durant
+ * @param int input speed
+ * @return int restricted speed
+ */
+int speedCheck(int speed);
+
+void setLeftMotor(int left);
+
+void setRightMotor(int right);
+
+void setMotors(int lm, int rm);
+
+void forwards(int s);
+
+void backwards(int s);
+
+void slowDown();
+
+void right();
+
+void left();
+
+/**
+ * Sets all motors to low (forwards and backwards)
+ * This allows the robot to freewheel
+ *
+ * @author Andrew Durant
+ */
+void freewheel();
+
+/**
+ * Sets all motors to high (forwards and backwards)
+ * This stops it from freewheeling at all
+ *
+ * @author Andrew Durant
+ */
+void brake();
+
+void spinLeft();
+
+void spinRight();
+
+#endif
