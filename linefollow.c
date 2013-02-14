6afbcc9e6b2eacbd1faf060a6f2ea232ff5bd70a
Chris King <cjk508@york.ac.uk>
2013-02-14 16:51:33 +0000

    todo list added for line follow

--- linefollow.c
+++ linefollow.c
@@ -6,10 +6,24 @@ void useRawSensors(void)
 {
   unsigned char *buf[10];
   uint32_t status = cmdRawSens(*buf);
+  /**
+  *@todo need to work out whether 0 is black or 1000
+  *      once this have been sussed out then it will 
+         attempt to look for a black floor. It will
+         then calibrate and use the line follow command
+         It will keep searching for the "barcode" throughout     
+  */
 }
 
 void useCalibratedSensors(void)
 {
   unsigned char *buf[10];
   uint32_t status = cmdCalSens(*buf);
+  /**
+  *@todo need to work out whether 0 is black or 2000
+  *      once this have been sussed out then it will 
+         attempt to look for a black floor. It will
+         then calibrate and use the line follow command
+         It will keep searching for the "barcode" throughout     
+  */  
 }
