67c7606e6c642b5af4e924bb861056c53cd8a64e
Lloyd Wallis <lpw503@pc767s.cs.york.ac.uk>
2013-02-26 13:45:43 +0000

    Changed parallel to wall movement to use rounded values that are less random

--- correctmotion.c
+++ correctmotion.c
@@ -6,31 +6,20 @@ void correctForwardMotion() {
   //Get an initial value
   SensorPair left = getLeftSensorValues();
   SensorPair right = getRightSensorValues();
-  //Other variables used
-  SensorPair x;
+  
   int use_left; //1 = left, 0 = right
-  /**
-  * Wait for a little while
-  * @todo Better way of doing this (e.g. interrupt/other sleep method)?
-  */
-  int i;
-  for (i = 0; i < 1000000000; i++);
 
   /**
   * Comparing the sets of values, choose either left or right
   * as a calibration pair - the closest one preferably
   * A higher sensor value means closer to an object
-  *
-  * Currently forced to always choose Right
   */
   if ((left.FrontSensor + left.RearSensor) / 2 >=
        (right.FrontSensor + right.RearSensor) /2) {
-    //The Left is closer to something, use that from now on
-    x = getLeftSensorValues();
+    //The Left is closer to something, use that
     use_left = 1;
   } else {
-    //The Right is closer to something, use that from now on
-    x = getRightSensorValues();
+    //The Right is closer to something, use that
     use_left = 0;
   }
 
@@ -40,11 +29,9 @@ void correctForwardMotion() {
   */
   if (
       //If using left and moving away from object, turn left (move away a bit)
-      (use_left
-       && ((left.FrontSensor + left.RearSensor)/2 > (x.FrontSensor + x.RearSensor))/2)
+      (use_left && left.FrontSensor < left.RearSensor)
       //If using right and moving toward, turn left
-      || (!use_left
-          && ((right.FrontSensor + right.RearSensor)/2 < (x.FrontSensor + x.RearSensor))/2)
+      || (!use_left && right.FrontSensor > right.RearSensor)
      ) {
        //Adjust right a bit, decide whether to speed up left or slow down right
        if (current_motor_speed_left > current_motor_speed_right) {
@@ -58,12 +45,9 @@ void correctForwardMotion() {
      } 
   else if (
       //If using left and moving toward an object, turn right (move toward a bit)
-      (use_left
-       && (left.FrontSensor + left.RearSensor)/2 < (x.FrontSensor + x.RearSensor)/2)
+      (use_left && left.FrontSensor > left.RearSensor)
       //If using right and moving away, turn right
-      || 
-      (!use_left
-       && (right.FrontSensor + right.RearSensor)/2 > (x.FrontSensor + x.RearSensor)/2)
+      || (!use_left && right.FrontSensor < right.RearSensor)
        ) {
          //Adjust right a bit, decide whether to speed up right or slow down left
          if (current_motor_speed_left > current_motor_speed_right) {
