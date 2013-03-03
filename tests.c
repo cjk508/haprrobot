f4216d7b1254e886265fe2dcd926dac543b268fa
Chris King <cjk508@york.ac.uk>
2013-03-03 17:11:26 +0000

    More Doxyvim tests.cvim tests.c

--- tests.c
+++ tests.c
@@ -19,11 +19,19 @@
 #include "correctmotion.h"
 #include "timer.h"
 
+/**
+ * @def	DBG_LEVEL
+ * Something to do with Andy... dunno what exactly :P
+ */
 #define DBG_LEVEL 1
 
 // Here be test functions!
 
-
+/**
+ * Tests the uart connections between the MBED board and the m3pi
+ *
+ * @author Andrew Durant
+ */
 void serialTest() {
   if (DBG_LEVEL >= 1) _DBG_("Init Serial");
 //  initSerial(); 
@@ -32,6 +40,11 @@ void serialTest() {
   _DBG_((char*)buf);
 }
 
+/**
+ * Tests the front and side sensors on the robot, prints out the values that correctmotion will recieve
+ *
+ * @author Christopher King
+ */
 void sensorsTest() {
   SensorPair left = getLeftSensorValues();
   SensorPair right = getRightSensorValues();
@@ -43,9 +56,11 @@ void sensorsTest() {
   int i;
   for (i = 0; i < 10000; i++);
 }
-
-
-
+/**
+ * Tests line following. Very easy atm because the linefollowing code doesn't work
+ *
+ * @author Christopher King
+ */
 
 void linefollowTest(){
   _DBG_("LINE FOLLOW TEST");
@@ -68,13 +83,19 @@ void linefollowTest(){
   //getCalibratedSensors(*sens); error stating undefined reference for both this function and getRawSensors(*sens);
 }
 
+/**
+ * Tests the motion correction, should stop if there is an object infront of the robot, otherwise it should follow a wall
+ *
+ * @author Lloyd Wallis
+ * @author Christoher King
+ */
 void motorCorrectTest() {
   forwards(15);
   while (1) 
   {
-//   while (getFrontSensorValue() == 1)    {
-//     brake();
-//   }    
+   while (getFrontSensorValue() == 1){
+	   brake();
+   }    
     correctForwardMotion();
   }
 }
