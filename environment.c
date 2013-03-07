7f10b4e12f6d5560bed81812f16113fad5201679
Chris King <cjk508@york.ac.uk>
2013-03-07 15:08:04 +0000

    Check for lines and walls created for state machine... available for all

---
+++ environment.c
@@ -0,0 +1,31 @@
+int checkForLine() {
+  uint16_t sensorPattern[5] = {0};
+  getRawSensors(&sensorPattern); // & is unnecessary we know... we are just being cautious
+  int i;
+  int isThereALine = 0;
+  
+  for (i = 0; i<5; i++) {
+    if (sensorPattern[i] == 2000)
+      isThereALine = 1;
+  }
+  if (isThereALine) {
+    return 1;
+  }
+  else
+    return 0;
+}
+
+
+int checkForWall() {
+  SensorPair leftSensors = calibratedValuesLeft(getLeftSensorValues());
+  SensorPair rightSensors = calibratedValuesRight(getRightSensorValues());  
+  if (leftSensors.FrontSensor < 40 || leftSensors.RearSensor < 40) {
+    setSensorSide(1);
+    return 1;
+  }
+  else if (rightSensors.FrontSensor < 40 || rightSensors.RearSensor < 40) {
+    setSensorSide(0);  
+    return 1;
+  }
+  return 0;
+}
